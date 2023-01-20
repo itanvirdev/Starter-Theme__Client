62199ad15e606dac6b825ed1630a5871e08f9aaf
itanvirdev <thisistanvir.dev@gmail.com>
2023-01-21 00:43:28 +0600

    Add theme basic files

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package starter
+ */
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+?>
+
+</main> <!-- #main -->
+
+
+<?php
+
+if (!function_exists('elementor_theme_do_location') || !elementor_theme_do_location('footer')) {
+  get_template_part('./template-parts/footer');
+}
+
+wp_footer(); ?>
+</body>
+
+</html>
